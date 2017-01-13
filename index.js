4b9ed90a72de25029e0bbe8806756fd192aba7d9
Jo Jangmi <wkdal720@gmail.com>
2017-01-13 15:40:17 -0800

    get posts

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ document.addEventListener('DOMContentLoaded', () => {
   if (window.sessionStorage.username) {
     connect.classList.add('hidden');
     requestPosts();
-    let refreshIn = window.sessionStorage.expiration_time - new Date().getTime() - 600;
+    let refreshIn = Number(window.sessionStorage.expiration_time) - new Date().getTime() - 600000;
     setRefreshTimeout(refreshIn);
   } else {
     connect.addEventListener('click', requestLogin);
     let url = new URL(window.location.href);
     if (url.search !== '' && !url.searchParams.get('error')) {
+      connect.classList.add('hidden');
       getAccessCode(url.searchParams);
     }
   }
