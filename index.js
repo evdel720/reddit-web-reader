3b64ca6055993fd63944c66bfdbf26201107c35c
Jo Jangmi <wkdal720@gmail.com>
2017-01-15 17:20:38 -0800

    mobile infinite scroll work

--- index.js
+++ index.js
@@ -1,33 +1,45 @@
-/* global requestLogin */
-/* global getAccessCode */
-/* global requestPosts */
-/* global setRefreshTimeout */
-/* global setSectionChangeHandlers */
 
 document.addEventListener('DOMContentLoaded', () => {
   let connect = document.getElementById('connect');
   let section = document.querySelector('ol');
+  let edit = document.getElementById('edit');
+
+  // to edit subreddits, takes user to the edit page
+  edit.addEventListener('click', (e) => {
+    e.preventDefault();
+    window.location = 'https://www.reddit.com/subreddits/';
+  });
+
   let expirationTime = Number(sessionStorage.getItem('expiration_time'));
   setSectionChangeHandlers();
+
+  // whenever page refreshes, the last should be initialized
   sessionStorage.removeItem('last');
 
-  if (!sessionStorage.getItem('section')) { sessionStorage.setItem('section', "hot"); }
+  if (!sessionStorage.getItem('section')) {
+    // setup the default section
+    sessionStorage.setItem('section', "hot");
+  }
 
   if (expirationTime &&
     expirationTime < new Date().getTime()) {
+    // if the token is already expired but still in storage,
+    // clear sessionStorage
     sessionStorage.clear();
   }
 
   if (sessionStorage.getItem('access_token')) {
-    showInterface(connect, section);
+    showInterface(connect, section, edit);
     requestPosts();
-    let refreshIn = expirationTime - new Date().getTime() - 600000;
+    // setup requesting refresh token (since it expires in 60 mins from the initial access request)
+    // it refreshed before 5 minutes
+    let refreshIn = expirationTime - new Date().getTime() - 300000;
     setRefreshTimeout(refreshIn);
   } else {
     connect.addEventListener('click', requestLogin);
     let url = new URL(window.location.href);
     if (url.search !== '' && !url.searchParams.get('error')) {
-      showInterface(connect, section);
+      showInterface(connect, section, edit);
       getAccessCode(url.searchParams);
     }
   }
--- todo_list.md
+++ todo_list.md
@@ -1,3 +1,8 @@
-
-add/delete/edit subreddits
+TODO
 mobile view again
+take care of vote success
+
+How to Setup
+1) in terminal,
+  $ serve
+2) go to localhost:3000
